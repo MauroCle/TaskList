413253454e14bde2729378f53a168329f88568d8
Mauro Clerici <Mauroclerici.7@gmail.com>
2024-07-06 12:20:44 -0300

    first commit

---
+++ Models/AppTaskListContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace TaskList.Models
+{
+    public partial class AppTaskListContext : DbContext
+    {
+        public AppTaskListContext()
+        {
+        }
+
+        public AppTaskListContext(DbContextOptions<AppTaskListContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Task> Tasks { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-9685F53\\SQLEXPRESS01; DataBase=AppTaskList;Integrated Security=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Task>(entity =>
+            {
+                entity.HasKey(e => e.IdTask)
+                    .HasName("PK__Task__9FCAD1C5E0B21F6B");
+
+                entity.Property(e => e.CreationDate)
+                    .HasColumnType("datetime")
+                    .HasColumnName("Creation_Date")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.TaskDescription)
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("Task_Description");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskList.Models
+{
+    public partial class Task
+    {
+        public int IdTask { get; set; }
+        public string? TaskDescription { get; set; }
+        public DateTime? CreationDate { get; set; }
+    }
+}
